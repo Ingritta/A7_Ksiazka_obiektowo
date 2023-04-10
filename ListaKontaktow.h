abb6ea59150246b3ae1d43668a345d8aa301e597
Ewelina Przeworska <ewelina.przeworska.programista@gmail.com>
2023-04-10 10:35:52 +0200

    Added contakt list

---
+++ ListaKontaktow.h
@@ -0,0 +1,19 @@
+#ifndef LISTAKONTAKTOW_H
+#define LISTAKONTAKTOW_H
+
+#include <iostream>
+#include "AdresatMenedzer.h"
+
+using namespace std;
+
+class ListaKontaktow {
+    AdresatMenedzer adresatMenedzer;
+public:
+    ListaKontaktow(string nazwaPlikuZAdresatami) : adresatMenedzer(nazwaPlikuZAdresatami) {
+        adresatMenedzer.wczytajAdresatowZalogowanegoUzytkownikaZPliku();
+    };
+};
+
+#endif
+
+
